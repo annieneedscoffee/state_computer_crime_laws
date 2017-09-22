118085a050d15658b3236b81980ba8d17122df43
annineedscoffee <char.anne.spencer@gmail.com>
2017-09-21 18:38:54 -0700

    crime laws

---
+++ startbootstrap-shop-item-gh-pages 2/js/finalproject.js
@@ -0,0 +1,91 @@
+var overview = {
+  photo: "https://docs.google.com/drawings/d/189lDbGJMwY_wAJMEZFCqhudsGeVzirXLCqjVxviM-iQ/pub?w=960&h=720",
+  blurb: "In addition to federal law, different states have enacted different laws on various different types of computer crime. Click above to see heat maps of states with phishing laws, denial of service attack laws, ransomeware laws, and hacking, malware, and trespass laws. Remember, even if a state doesn't have a law that directly addresses these crimes federal law as well as other state law may still apply.",
+  name: "State Computer Laws"
+
+}
+
+var statesWithHackingMalwareAndTrespassLaws = {
+  photo: "https://docs.google.com/drawings/d/189lDbGJMwY_wAJMEZFCqhudsGeVzirXLCqjVxviM-iQ/pub?w=960&h=720",
+  imagecredit: "Image created using Google Docs U.S. Map template and data from NCSL's website",
+name: "States with Anti-Malware, Hacking, and Trespass Laws",
+  states: "AL, AK, AZ, AR, CA, CO, CT, DE, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OR, OK, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI WY.",
+  otherlaws: "Federal laws forbidding this behavior also apply to all states. Other laws may also apply in these states and others depending on circumstances, such as laws against steeling trade secrets and laws against stalking, for example.",
+getawaywithit: "No, you can't get away with this even in states or territories without laws directly addressing hacking, computer trespass, and malware. Other laws still apply.",
+  researchlastupdated: "Research last updated December 5, 2016",
+disclaimer: "This is not legal advice. Consult your lawyer.",
+  citations: "National Conference of State Legislatures(NCSL) available at http://www.ncsl.org/research/telecommunications-and-information-technology/computer-hacking-and-unauthorized-access-laws.aspx, Ala. Code §§ 13A-8-112, 13A-8-113, Ala. Code §§ 13A-8-112, 13A-8-113, Alaska Stat. § 11.46.740, Ariz. Rev. Stat. §§ 13-2316,13-2316.01,13-2316.02, Ark. Code §§ 5-41-101 to -206, Cal. Penal Code § 502, Colo. Rev. Stat. § 18-5.5-101 to -102, Conn. Gen. Stat. § 53a-250 to 53a-261, Del. Code tit., 11,§ 931 to 941, Fla. Stat. § 815.01 to 815.07, §§668.801to .805, Ga. Code §§ 16-9-90 to 16-9-94,§§ 16-9-150 to 16-9-157, Hawaii Rev. Stat. §§ 708-890 to 708-895.7, Idaho Code §18-2201, § 18-2202, 720 ILCS § 5/17-50 to -55, Ind. Code §§ 35-43-1-4, 35-43-2-3, Iowa Code § 716.6B, Kan. Stat. Ann. § 21-5839, Ky. Rev. Stat.§§434.840, 434.845, 434.850, 434.851, 434.853, 434.855, 434.860, La. Rev. Stat.Ann.§§ 14:73.1 to 14:73.8, Me. Rev. Stat. Ann. tit. 17-A, § 431 to 435, Md. Code, Crim. Law §7-302, Mass. Gen. Laws Ann.ch. 266, § 33A, Mich. Comp. Laws §§ 752.791, 752.792, 752.793, 752.794, 752.795, 752.796, 752.797, Minn. Stat. §§ 609.87 to 609.893, Miss. Code § 97-45-1 to 97-45-33, Mo. Rev. Stat.§ 537.525,§ 569.095,§ 569.097,§ 569.099, Mont. Code Ann.§ 45-2-101,§ 45-6-310,§ 45-6-311, Neb. Rev. Stat.§§ 28-1341 to28-1348, Nev. Rev. Stat. § 205.473 to 205.513, N.H. Rev. Stat. Ann. §§638:16, 638:17, 638:18,638:19, N.J. Rev. Stat.§§ 2A:38A-1 to -3, § 2C:20-2, §§ 2C:20-23 to 34, N.M. Stat. § 30-45-1 to 30-45-7, N.Y. Penal Law § 156.00 to 156.50, N.C. Gen. Stat. § 14-453 to 14-458, N.D. Cent. Code § 12.1-06.1-08, Ohio Rev. Code §§ 2909.01, 2909.04, 2909.07(A)(6), 2913.01 to 2913.04, Okla. Stat. tit.21, §§1951 to 1959, Or. Rev. Stat. § 164.377, 18 Pa. Stat. § 5741 to 5749, R.I. Gen. Laws § 11-52-1 to 11-52-8, S.C. Code § 16-16-10 to 16-16-40, S.D. Cod. Laws § 43-43B-1 to § 43-43B-8, Tenn. Code §§39-14-601 to -605, Tex. Penal Code § 33.02, Utah Code § 76-6-702 to 76-6-705, Vt. Stat.Ann. tit. 13, § 4101 to 4107, Va. Code§§ 18.2-152.1 to-152.15,§ 19.2-249.2, Wash. Rev. Code§ 9A.90.010 et seq., W. Va. Code §§ 61-3C-3 to 61-3C-21, Wis. Stat. § 943.70, Wyo. Stat. § 6-3-501 to § 6-3-506, §40-25-101"
+}
+
+var statesWithDenialOfServiceAttackLaws = {
+photo: "https://docs.google.com/drawings/d/1JH6N4oQCb2jESkdSS5r1FXHpBsjaWoDqr131rI_rWhc/pub?w=960&h=720",
+  imagecredit: "Image created using Google Docs U.S. Map template and data from NCSL's website",
+name: "States with Denial of Service Attack Laws",
+states: "AL, AZ, AR, CA, CT, DE, FL, GA, IL, IN, LA, MS, MO, NV, HN, NC, OH, OK, PA, SC, TN, VA, WA, WV, WY.",
+otherlaws: "Federal laws forbidding this behavior also apply to all states. Other laws may also apply in these states and others depending on circumstances, such as laws against computer trespass or hacking, for example.",
+getawaywithit: "No, you can't get away with this even in states or territories without laws directly addressing denial of service attacks. Other laws still apply.",
+researchlastupdated: "Research last updated December 5, 2016",
+disclaimer: "This is not legal advice. Consult your lawyer.",
+citations: "National Conference of State Legislatures(NCSL) available at http://www.ncsl.org/research/telecommunications-and-information-technology/computer-hacking-and-unauthorized-access-laws.aspx, Ala. Code § 13A-8-112(5), Ariz. Rev. Stat. § 13-2316(4), Ark. Code § 5-41-203(a), Cal. Penal Code § 502, Conn. Gen. Stat. § 53a-251, Del. Code tit. 11, § 934, Fla. Stat. § 815.06, Ga. Code § 16-9-93, 720 ILCS § 5/17-51, Ind. Code § 35-43-1-8, La. Rev. Stat. Ann. § 14:73-4, Miss. Code § 97-45-5, Mo. Rev. Stat. § 569.099, Nev. Rev. Stat. § 205.477, N.H. Rev. Stat. Ann. § 638:17, N.C. Gen. Stat. § 14-456, 14-456.1, Ohio Rev. Code § 2909.01, Okla. Stat. tit. 21, § 1953, 18 Pa. C.S.A. § 7612, S.C. Code § 16-16-10, Tenn. Code § 39-14-601, Va. Code § 18.2-152.4, Wash. Rev. Code § 9A.90.060, W. Va. Code § 61-3C-8, Wyo. Stat. § 6-3-504"
+}
+
+var statesWithRansomwareLaws = {
+photo: "https://docs.google.com/drawings/d/1gsay8_CbE3jTzO9zTyBedWu--GgqyeQZ7UJ9tuJ0v7g/pub?w=960&h=720",
+  imagecredit: "Image created using Google Docs U.S. Map template and data from NCSL's website",
+  name: "States with Ransomware Laws",
+  states: "CA, WY.",
+  otherlaws: "Federal laws forbidding this behavior also apply to all states. Other laws may apply depending on circumstances, such as laws against extortion, malware, and computer trespass, for example.",
+getawaywithit: "No, you can't get away with this even in states or territories without laws directly addressing ransomeware. Other laws still apply.",
+researchlastupdated:"Research last updated December 5, 2016",
+disclaimer: "This is not legal advice. Consult your lawyer.",
+citations: "National Conference of State Legislatures(NCSL) available at http://www.ncsl.org/research/telecommunications-and-information-technology/computer-hacking-and-unauthorized-access-laws.aspx, Calif. Penal Code § 523 (2016 S.B. 1137), Wyo. Stat. § 6-3-506"
+}
+
+var statesWithPhishingLaws = {
+photo:"https://docs.google.com/drawings/d/1lqkHSixbxYlbnnBDUNxWhfgYYH8IDXS2aQaTQjHuC0A/pub?w=960&h=720",
+  imagecredit: "Image created using Google Docs U.S. Map template and data from NCSL's website",
+name: "States with Phishing Laws",
+states: "AL, AR, AZ, CA, CT, FL, GA, GU, IL, KY, LA, MI, MN, MT, NM, NY, OK, OR, RI, TN, TX, UT, VA, WA.",
+otherlaws: "Other laws may apply depending on circumstances, such as laws against identity theft, for example. Remember federal law also applies to all states.",
+getawaywithit: "No, you can't get away with this even in states or territories without laws directly addressing phishing. Other laws still apply, such as laws against identity theft and fraud, for example.",
+researchlastupdated: "Research last updated February 28, 2017",
+disclaimer: "This is not legal advice. Consult your lawyer.",
+citations: "National Conference of State Legislatures(NCSL) available at http://www.ncsl.org/research/telecommunications-and-information-technology/state-phishing-laws.aspx, Ala. Code §13A-8-114, Ark. Code §§ 4-111-102, 4-111-103, Ariz. Rev. Stat. §§ 18-541 to -544, Cal. Bus & Prof. Code §§ 22948 to 22948.3, Conn. Gen. Stat. § 53-454, Fla. Stat. §§ 668.701-.705, Ga. Code § 16-9-109.1, 740 ILCS §§ 7/1 - 7/15, Ky. Rev. § Stat. 434.697, La. Rev. Stat. §§ 51:2021 et seq., MCL § 445.67a, Minn. Stat. § 609.527, Subd. 5a, Mont. Code Ann. §§ 30-14-1712, 33-19-410, N.M. Stat. § 30-16-24.1, N.Y. Gen. Bus. § 390-b, Okla. Stat. tit. 15, §§ 776.8 - 776.12, Ore. Rev. Stat. § 646.A.808, R.I. Gen. Laws §§ 11-52.1-1 to -5, Tenn. Code §§ 47-18-5201 to 47-18-5205, Tex. Business and Commerce Code §§ 325.001 - .006, Utah Code §§ 13-40-201 to -204, -401, Virginia Code Ann. § 18.2-152.5:1, Wash. Rev. Code §§ 19.190.080 -090 -100, 5 GCA §§ 32703, 32704 (h)"
+}
+
+var stateComputerLaws = [overview, statesWithHackingMalwareAndTrespassLaws,
+statesWithDenialOfServiceAttackLaws, statesWithRansomwareLaws, statesWithPhishingLaws]
+
+function changeLawType(typeLaw){
+  console.log(typeLaw)
+  var image = document.getElementById("image");
+  var blurb = document.getElementById("blurb");
+  var name = document.getElementById("name");
+
+  if(typeLaw === 1){
+    image.src = stateComputerLaws[0].photo;
+    blurb.innerText = stateComputerLaws[0].blurb;
+    caption.innerText = stateComputerLaws[0].name;
+
+  } else if (typeLaw === 2){
+    image.src = stateComputerLaws[1].photo;
+    blurb.innerText = stateComputerLaws[1].states + " " + stateComputerLaws[1].otherlaws + " " + stateComputerLaws[1].getawaywithit + " " + stateComputerLaws[1].researchlastupdated + " " + stateComputerLaws[1].disclaimer + " " + stateComputerLaws[1].imagecredit + " " + stateComputerLaws[1].citations;
+    caption.innerText = stateComputerLaws[1].name;
+
+  } else if (typeLaw === 3){
+    image.src = stateComputerLaws[2].photo;
+    blurb.innerText = stateComputerLaws[2].states + " " + stateComputerLaws[2].otherlaws + " " + stateComputerLaws[2].getawaywithit + " " + stateComputerLaws[2].researchlastupdated + " " + stateComputerLaws[2].disclaimer + " " + stateComputerLaws[2].imagecredit + " " + stateComputerLaws[2].citations;
+    caption.innerText = stateComputerLaws[2].name;
+
+  } else if (typeLaw === 4){
+    image.src = stateComputerLaws[3].photo;
+    blurb.innerText = stateComputerLaws[3].states + " " + stateComputerLaws[3].otherlaws + " " + stateComputerLaws[3].getawaywithit + " " + stateComputerLaws[3].researchlastupdated + " " + stateComputerLaws[3].disclaimer + " " + stateComputerLaws[3].imagecredit + " " + stateComputerLaws[3].citations;
+    caption.innerText = stateComputerLaws[3].name;
+
+  } else if  (typeLaw === 5){
+    image.src = stateComputerLaws[4].photo;
+    blurb.innerText = stateComputerLaws[4].states + " " + stateComputerLaws[4].otherlaws + " " + stateComputerLaws[4].getawaywithit + " " + stateComputerLaws[4].researchlastupdated + " " + stateComputerLaws[4].disclaimer + " " + stateComputerLaws[4].imagecredit + " " + stateComputerLaws[4].citations;
+    caption.innerText = stateComputerLaws[4].name;
+
+  }
+}
